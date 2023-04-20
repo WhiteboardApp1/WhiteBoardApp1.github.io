f176331e6b207d1caedd818b6d65702d342ad99e
RobotDaniel <Daniel_burridge@outlook.com>
2023-04-20 17:34:04 +0100

    added align buttons

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 document.getElementById('myInput').addEventListener('input', function() {
-    this.style.fontSize = '5vh';
-    this.style.width = 'auto';
-    this.style.height = 'auto';
-    this.style.width = (this.scrollWidth + 10) + 'px'; // Add 10px for padding
-    this.style.height = (this.scrollHeight + 10) + 'px'; // Add 10px for padding
-  });
-  
+  this.style.fontSize = '5vh';
+  this.style.width = 'auto';
+  this.style.height = 'auto';
+  this.style.width = (this.scrollWidth + 10) + 'px'; // Add 10px for padding
+  this.style.height = (this.scrollHeight + 10) + 'px'; // Add 10px for padding
+});
+
+document.getElementById('alignLeftBtn').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent form submission
+  document.getElementById('myInput').style.textAlign = 'left';
+});
+
+document.getElementById('alignCenterBtn').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent form submission
+  document.getElementById('myInput').style.textAlign = 'center';
+});
+
+document.getElementById('alignRightBtn').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent form submission
+  document.getElementById('myInput').style.textAlign = 'right';
+});
